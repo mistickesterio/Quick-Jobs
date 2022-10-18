1e97b28862c50a6c169a2382db336b577913f38b
mistickesterio <thiagosilvadacruz163@gmail.com>
2022-10-18 20:42:54 -0300

    first commit

---
+++ Quick-Jobs database.sql
@@ -0,0 +1,38 @@
+create DATABASE Quick_Jobs;
+USE Quick_Jobs;
+
+CREATE TABLE Vagas(
+    ID INT NOT NULL AUTO_INCREMENT,
+    Descricao VARCHAR(255),
+    Titulo VARCHAR(255),
+    Empresa VARCHAR(255),
+    Icone nvarchar,
+    Salario VARCHAR(255),
+    Cidade VARCHAR(255),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Area_vagas(
+    ID VARCHAR(2) NOT NULL,
+    URL VARCHAR(255),
+    PRIMARY KEY(iD)
+);
+
+INSERT INTO Cliente (Nome,CPF) VALUES
+('Lucas', '12312312312'),
+('Luiz', '32132132132'),
+('Lourival', '12312312312'),
+('Otavio', '32132132132'),
+('Kerolaine', '32132132132'),
+('Thiago', '34534534534');
+
+INSERT INTO Produto (Nome,ID) VALUES
+('Pipoca', '01'),
+('Docê de Leite', '02'),
+('Manteiga', '03'),
+('Pastél', '04'),
+('Coca Cola', '05'),
+('Cachorro quente', '06');
+
+
+
