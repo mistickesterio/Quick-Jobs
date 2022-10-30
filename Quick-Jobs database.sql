fc67bee9953bd6f381591901708428cd64837b05
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-10-30 15:12:48 -0300

    FORTH COMMIT

--- Quick-Jobs database.sql
+++ Quick-Jobs database.sql
@@ -21,15 +21,8 @@ CREATE TABLE Area_vagas(
 INSERT INTO Vagas (Descricao, CPF) VALUES
 ('https://www.linkedin.com/jobs/view/3319869254', '12312312312'),
 ('https://www.linkedin.com/jobs/view/3312861995', '32132132132'),
-('Lourival', '12312312312'),
-('Otavio', '32132132132'),
-('Kerolaine', '32132132132'),
-('Thiago', '34534534534');
+('https://www.linkedin.com/jobs/view/3312861995', '32132132132');
 
-INSERT INTO Produto (Nome,ID) VALUES
+INSERT INTO Area_vagas (ID, URL) VALUES
 ('Pipoca', '01'),
-('Docê de Leite', '02'),
-('Manteiga', '03'),
-('Pastél', '04'),
-('Coca Cola', '05'),
 ('Cachorro quente', '06');
