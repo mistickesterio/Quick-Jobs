b9808c0bb44ed260c5f9bce95fd2c43e23d061a5
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-11-01 15:10:39 -0300

    commit?
    Conexão com banco de dados.

--- Quick-Jobs database.sql
+++ Quick-Jobs database.sql
@@ -1,28 +1,38 @@
-create DATABASE Quick_Jobs;
-USE Quick_Jobs;
+-- derruba banco de dados.
+drop DATABASE QuickJobs;
+-- cria banco de dados.
+CREATE DATABASE QuickJobs;
+USE QuickJobs;
 
-CREATE TABLE Vagas(
-    ID INT NOT NULL AUTO_INCREMENT,
-    Descricao VARCHAR(255),
-    Titulo VARCHAR(255),
-    Empresa VARCHAR(255),
-    Icone nvarchar,
-    Salario VARCHAR(255),
-    Cidade VARCHAR(255),
-    PRIMARY KEY (ID)
+-- primeira tabela.
+CREATE TABLE Area(
+id int not null AUTO_INCREMENT primary key,  
+nome varchar(255) not null
 );
 
-CREATE TABLE Area_vagas(
-    ID VARCHAR(2) NOT NULL,
-    URL VARCHAR(255),
-    PRIMARY KEY(iD)
+-- segunda tabela.
+CREATE TABLE Vaga(
+	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Area int not null,
+	Descricao VARCHAR(255) not null,
+	Titulo VARCHAR(255) not null,
+    Empresa VARCHAR(255) not null,
+    Icone LONGTEXT,
+	Salario VARCHAR(255),
+    Cidade VARCHAR(255) not null,
+    url VARCHAR(255) not null,
+    Foreign key (Area) references area (id)
 );
 
-INSERT INTO Vagas (Descricao, CPF) VALUES
-('https://www.linkedin.com/jobs/view/3319869254', '12312312312'),
-('https://www.linkedin.com/jobs/view/3312861995', '32132132132'),
-('https://www.linkedin.com/jobs/view/3312861995', '32132132132');
+-- primeiro insert.
+INSERT INTO Area(nome) VALUES
+("frontend"),
+("backend"),
+("testes");
 
-INSERT INTO Area_vagas (ID, URL) VALUES
-('Pipoca', '01'),
-('Cachorro quente', '06');
+-- segundo insert.
+INSERT INTO Vaga(ID, Area, Descricao, Titulo, Empresa, Icone, Salario, Cidade, url) VALUES
+(DEFAULT, DEFAULT, 'Originating in Brazil, we also work in Argentina and we have the ambition to win the world. We propel the people and the country with energy solutions. Energy is our business, and is in our name.',
+'Product Owner', 'Raigen',  DEFAULT, 'Não visivel', "Santana", 'https://www.linkedin.com/jobs/view/3319869254'),
+(DEFAULT, DEFAULT, 'Acreditamos que os indivíduos e suas interações fazem o seu e o nosso negócio. Este é o nosso jeito de fazer: com tecnologia e simplicidade.',
+ 'Product Owner', 'Meta', DEFAULT, 'Não visivel', "Santana", 'https://www.linkedin.com/jobs/view/3312861995');
